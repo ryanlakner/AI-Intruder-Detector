6c584bbdb0b546c9dc56cf057d3666ecba1e79e4
Ryan Lakner <95839458+ryanlakner@users.noreply.github.com>
2022-09-22 14:14:25 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -7,15 +7,18 @@ from cvzone.SerialModule import SerialObject
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 arduino = SerialObject(digits=3)
 
+
+# Lowering Resolution for better computation
 def make_240p():
-    cap.set(3, 360)
+    cap.set(3, 320)
     cap.set(4, 240)
 
+# Calling lower res function
 make_240p()
-    
+
 xPID = PID([0.033, 0, 0.02], 640 // 2, axis=0)
 yPID = PID([0.03, 0, 0.02], 480 // 2 - 40, axis=1)
 
@@ -48,17 +51,17 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                               landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
                               landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
 
-            center_x = (landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x +
-                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x) / 2
-            center_y = (landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y +
-                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y) / 2
-            center_z = (landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].visibility +
-                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility) / 2
+            center_x = ((landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x) + (
+                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x)) / 2
+            center_y = ((landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y) + (
+                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y)) / 2
+            center_z = ((landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].visibility) + (
+                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility)) / 2
             center_chest = [center_z, center_x, center_y]
 
             a = np.array(center_chest)
 
-            if a[0] > 0.8:
+            if (a[0] > 0.8):
                 human_detected = 1
 
                 cx = a[1]  # cx = x
@@ -72,17 +75,16 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                 xAngle -= resultX
                 yAngle += resultY
 
-                image = xPID.draw(image, [cx_converted, cy_converted])
-                image = yPID.draw(image, [cx_converted, cy_converted])
+                # image = xPID.draw(image, [cx_converted, cy_converted])
+                # image = yPID.draw(image, [cx_converted, cy_converted])
 
         except:
             human_detected = 0
             pass
 
-        # Render detections
-        print(human_detected)
+        # print(human_detected)
         arduino.sendData([xAngle, yAngle, human_detected])
-        cv2.imshow('Mediapipe Feed', image)
+        # cv2.imshow('Camera Feed', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
