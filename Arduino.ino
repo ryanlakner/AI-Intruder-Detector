89fe6a3695d0e6befd2e1c540ad3961d8d5307d9
Ryan Lakner <95839458+ryanlakner@users.noreply.github.com>
2022-11-10 15:52:57 -0600

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -21,9 +21,9 @@ LiquidCrystal_I2C lcd(0x27, 16 ,2);
 //Password setup
 char data[Password_Length];
 char master[Password_Length] = "1234";
-byte data_count = 0;
-byte data_count_cur_pw = 0;
-byte data_count_test = 0;
+byte data_system1_count = 0;
+byte data_system2_count = 0;
+byte data_master_count = 0;
 char custom_key;
 
 //Keypad setup
@@ -37,6 +37,7 @@ char hexaKeys[rows][cols] ={
 };
 byte row_pins[rows] = {9,8,7,6};
 byte col_pins[cols] = {5,4,3,2};
+
 //keypad object
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), row_pins, col_pins, rows, cols);
 
@@ -46,19 +47,13 @@ Servo servoVer;
 
 //delay function
 unsigned long startTime = 0;
-unsigned long interval = 500;   //interval_led
+unsigned long interval = 500;   //led interval
 unsigned long currentTime;
 
 //system status
 int System = 0;
 
-//Function for LCD to display
-void lcd_password_setup()
-{
-  lcd.setCursor(1,0);
-  lcd.print("Enter Password");
-}
-
+//Functions for LCD to display
 void lcd_start_setup()
 {
   lcd.setCursor(1,0);
@@ -67,13 +62,19 @@ void lcd_start_setup()
   lcd.print("Press C To CPW");
 }
 
-void lcd_changepw_setup()
+void lcd_enter_password()
+{
+  lcd.setCursor(1,0);
+  lcd.print("Enter Password");
+}
+
+void lcd_new_password()
 {
   lcd.setCursor(1,0);
   lcd.print("New Password:");
 }
 
-void lcd_ask_current_pw_setup()
+void lcd_current_password()
 {
   lcd.setCursor(0,0);
   lcd.print("Current Password:");
@@ -82,28 +83,26 @@ void lcd_ask_current_pw_setup()
 //LCD Display
 unsigned long lcd_show_correct = 0;
 unsigned long lcd_show_incorrect = 0;
+unsigned long lcd_show_succeed = 0;
 int lcd_state;
-/*state 0 : Idle
- *state 1 : Correct
- *state 2 : Incorrect
- */
 
-void clearData()
+//Functions to clear entries in data
+void clearData1()
 {
   // Go through array and clear data
-  while (data_count != 0)
+  while (data_system1_count != 0)
   {
-    data[data_count--] = 0;
+    data[data_system1_count--] = 0;
   }
   return;
 }
 
-void clearDataCur()
+void clearData2()
 {
   // Go through array and clear data
-  while (data_count_cur_pw != 0)
+  while (data_system2_count != 0)
   {
-    data[data_count_cur_pw--] = 0;
+    data[data_system2_count--] = 0;
   }
   return;
 }
@@ -116,12 +115,14 @@ void setup()
   pinMode(LED,OUTPUT);
   servoHor.attach(10);
   servoVer.attach(11);
-  lcd_start_setup(); //Calling LCD Setup
+  lcd_start_setup();
 }
 
 void loop()
 {
   currentTime = millis();
+
+  //Servos receive data from pycharm
   serialDetection.Get(vals_Dec_Rec);
   servoHor.write(vals_Dec_Rec[0]);
   servoVer.write(vals_Dec_Rec[1]);
@@ -137,47 +138,63 @@ void loop()
     lcd_state = 0;
     System = 1;
     lcd.clear();
-    lcd_password_setup();
+    lcd_enter_password();
     }
 
+    //Press C to change password
     else if(custom_key == 'C' && System == 0)
     {
     lcd_state = 0;
     System = 2;
     lcd.clear();
-    lcd_ask_current_pw_setup();
+    lcd_current_password();
     }
 
+    //When system is ARMED
+    //Store inputs to variable data
     else if(System == 1)
     {
-    data[data_count] = custom_key;
-    lcd.setCursor(data_count,1);
-    //lcd.print(data[data_count]);
+    data[data_system1_count] = custom_key;
+    lcd.setCursor(data_system1_count,1);
     lcd.print("*");
-    data_count++;
-    }
-    
-    else if(System == 2)
-    {
-    data[data_count_cur_pw] = custom_key;
-    lcd.setCursor(data_count_cur_pw,1);
-    lcd.print("*");
-    data_count_cur_pw++;
+    data_system1_count++;
     }
 
+    //Entering changing password state when system is UNARM
+    //Store inputs to variable data
+    else if(System == 2)
+    {
+      //Go back to initial if press C again
+      if(custom_key == 'C')
+      {
+        System = 0;
+        lcd.clear();
+        lcd_start_setup();
+      }
+      else
+      {
+      data[data_system2_count] = custom_key;
+      lcd.setCursor(data_system2_count,1);
+      lcd.print("*");
+      data_system2_count++;
+      }
+    }
+
+    //Changing master password
     else if(System == 3)
     {
-      master[data_count_test] = custom_key;
-      lcd.setCursor(data_count_test,1);
+      master[data_master_count] = custom_key;
+      lcd.setCursor(data_master_count,1);
       lcd.print("*");
-      data_count_test++;
+      data_master_count++;
     }
   }
 
-  //Checking password
-  if (data_count_cur_pw == Password_Length - 1)
+  //checking data with current password before changing to new password
+  if (data_system2_count == Password_Length - 1)
   {
     lcd.clear();
+    //if data match current password then the system is ready to change master password
     if (!strcmp(data, master))
     {
       System = 3;
@@ -191,21 +208,26 @@ void loop()
       lcd_show_incorrect = currentTime;
       lcd_state = 4;
     }
-    clearDataCur();
+    //If data exceed 4 characters then clear data
+    clearData2();
   }
-  
-  if (data_count_test == Password_Length - 1)
+
+  //if master password has 4 character then reset everything to initial state
+  if (data_master_count == Password_Length - 1)
   {
     System = 0;
-    lcd_state = 0;
     lcd.clear();
-    lcd_start_setup();
-    data_count_test = 0;
+    lcd.print("    Succeed");
+    lcd_show_succeed = currentTime;
+    lcd_state = 5;
+    data_master_count = 0;
   }
-  
-  if (data_count == Password_Length - 1)
+
+  //When system is ARMED, check data with master to DISARM
+  if (data_system1_count == Password_Length - 1)
   {
     lcd.clear();
+    //if data match master password then the system is UNARM
     if (!strcmp(data, master))
     {
       System = 0;
@@ -219,10 +241,12 @@ void loop()
       lcd_show_incorrect = currentTime;
       lcd_state = 2;
     }
-    clearData();
+    //If data exceed 4 characters then clear data
+    clearData1();
   }
 
-  //Displaying result for 3 sec on lcd
+  //Displaying result for 3s on lcd
+  //Show correct after entering correct password and go to set up screen
   if(lcd_state == 1)
   {
     if(currentTime - lcd_show_correct >= 2000)
@@ -231,27 +255,41 @@ void loop()
     }
   }
 
+  //show incorrect after entering wrong password and keep asking for password
   if(lcd_state == 2)
   {
     if(currentTime - lcd_show_incorrect >= 2000)
     {
-      lcd_password_setup();
+      lcd_enter_password();
     }
   }
-  
+
+  //Show correct after entering correct current password and ask for new password
   if(lcd_state == 3)
   {
     if(currentTime - lcd_show_correct >= 2000)
     {
-      lcd_changepw_setup();
+      lcd_new_password();
     }
   }
-  
+
+  //Show incorrect after entering wrong current password and keep asking for current password
   if(lcd_state == 4)
   {
     if(currentTime - lcd_show_incorrect >= 2000)
     {
-      lcd_ask_current_pw_setup();
+      lcd_current_password();
+    }
+  }
+
+  //Show succeed after new password is entered and go to set up screen
+  if(lcd_state == 5)
+  {
+    if(currentTime - lcd_show_succeed >= 2000)
+    {
+      lcd_state = 0;
+      lcd.clear();
+      lcd_start_setup();
     }
   }
   
@@ -260,6 +298,7 @@ void loop()
   {
     if(vals_Dec_Rec[2]>0)
     {
+      //Every 0.5s, changing the state of the LED
       if(currentTime - startTime >= interval)
       {
         startTime = currentTime;
